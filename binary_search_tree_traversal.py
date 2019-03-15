2a5f72db05bd14d35ac4d83558b1132ad3736f59
Velu <velarun227@gmail.com>
2019-03-15 18:46:04 +0530

    Update binary_search_tree_traversal.py

--- binary_search_tree_traversal.py
+++ binary_search_tree_traversal.py
@@ -5,7 +5,7 @@ class Node:
         self.right = None
         self.data = data
         
-# Insert Node of BST
+    # Insert Node of BST
     def insert(self, data):
 
         if self.data:
@@ -23,45 +23,81 @@ class Node:
             self.data = data
 
 # Print the Tree
-    def PrintTree(self, root, ns, ch):
-        if root == None:
-            return 
+def PrintTree(root, ns, ch):
+    if root == None:
+        return 
 
-        string2=ch + ":" + " " + str(root.data)
-        print(string2.rjust(ns))
-        
-        self.PrintTree(root.left, ns+10, "L")
-        self.PrintTree(root.right, ns+10, "R")
+    string2=ch + ":" + " " + str(root.data)
+    print(string2.rjust(ns))
+    
+    PrintTree(root.left, ns+10, "L")
+    PrintTree(root.right, ns+10, "R")
         
 # Inorder traversal
 # Left -> Root -> Right
-    def inorderTraversal(self, root):
-        res = []
-        if root:
-            res = self.inorderTraversal(root.left)
-            res.append(root.data)
-            res = res + self.inorderTraversal(root.right)
-        return res
+def inorderTraversal(root):
+    res = []
+    if root:
+        res = inorderTraversal(root.left)
+        res.append(root.data)
+        res = res + inorderTraversal(root.right)
+    return res
         
 # Preorder traversal
 # Root -> Left ->Right        
-    def PreorderTraversal(self, root):
-        res = []
-        if root:
-            res.append(root.data)
-            res = res + self.PreorderTraversal(root.left)
-            res = res + self.PreorderTraversal(root.right)
-        return res
+def PreorderTraversal(root):
+    res = []
+    if root:
+        res.append(root.data)
+        res = res + PreorderTraversal(root.left)
+        res = res + PreorderTraversal(root.right)
+    return res
         
 # Postorder traversal
 # Left ->Right -> Root
-    def PostorderTraversal(self, root):
-        res = []
-        if root:
-            res = self.PostorderTraversal(root.left)
-            res = res + self.PostorderTraversal(root.right)
-            res.append(root.data)
-        return res    
+def PostorderTraversal(root):
+    res = []
+    if root:
+        res = PostorderTraversal(root.left)
+        res = res + PostorderTraversal(root.right)
+        res.append(root.data)
+    return res 
+        
+# Function to  print level order traversal of tree 
+def printLevelOrder(root): 
+    h = height(root)
+    out = []
+    for i in range(1, h+1): 
+        printGivenLevel(root, i, out)
+    return out
+    
+# Print nodes at a given level 
+def printGivenLevel(root , level, out): 
+    if root is None: 
+        return
+    if level == 1: 
+        out.append(root.data)
+    elif level > 1 : 
+        printGivenLevel(root.left , level-1, out) 
+        printGivenLevel(root.right , level-1, out) 
+  
+""" Compute the height of a tree--the number of nodes 
+    along the longest path from the root node down to 
+    the farthest leaf node 
+"""
+def height(node): 
+    if node is None: 
+        return 0 
+    else : 
+        # Compute the height of each subtree  
+        lheight = height(node.left) 
+        rheight = height(node.right) 
+  
+        #Use the larger one 
+        if lheight > rheight : 
+            return lheight+1
+        else: 
+            return rheight+1
         
 root = Node(27)
 root.insert(14)
@@ -71,7 +107,13 @@ root.insert(19)
 root.insert(31)
 root.insert(42)
 
-root.PrintTree(root, 0, "M")
-print("Inorder Traversal: ", root.inorderTraversal(root))
-print("Preorder Traversal: ", root.PreorderTraversal(root))
-print("Postorder Traversal: ", root.PostorderTraversal(root))
+print("Tree:")
+PrintTree(root, 0, "M")
+
+print("DFS Traversal:")
+print("Inorder Traversal: ", inorderTraversal(root))
+print("Preorder Traversal: ", PreorderTraversal(root))
+print("Postorder Traversal: ", PostorderTraversal(root))
+
+print("BFS Traversal:")
+print("Level order traversal:", printLevelOrder(root))
